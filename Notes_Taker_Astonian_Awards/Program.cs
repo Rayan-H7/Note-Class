ddc7c4ee23d9acda366206f2d4b6422687acbe46
Rayan Hassan <98430041+Rayan-H7@users.noreply.github.com>
2022-04-16 16:02:28 +0100

    Add files via upload

---
+++ Notes_Taker_Astonian_Awards/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Notes_Taker_Astonian_Awards
+{
+    class Program
+    {
+        static void __init__(string[] args)
+        {
+            Console.WriteLine ("What would you like to note down? ");
+            string text = Console.ReadLine();
+            note Note1 = new note();
+            Note1.content = text;
+            Console.WriteLine("Check for completion: 1 = Completed");
+            Console.WriteLine("Check for completion: 2 = Half-Way There");
+            Console.WriteLine("Check for completion: 3 = Not Completed At All");
+            Console.Write("How far have you completed "+Note1+"?");
+            int completion_check_digit = Convert.ToInt16(Console.ReadLine());
+            Note1.check(completion_check_digit);
+            /* This project is an application that allows you to write notes
+            */
+        }
+    }
+}
--- Notes_Taker_Astonian_Awards/note_manager.cs
+++ Notes_Taker_Astonian_Awards/note_manager.cs
@@ -1,37 +1,40 @@
 using System;
 
-public class note{
-
-    public static __init__(string note_){
-        string notes = note_;
-        int char_count = notes.Length;
-
-        if (char_count>50){
-            Console.WriteLine("The note is beyond the character limits");
-            note = "Nothing"; 
-            break;
-        } else
-        {
-            Console.WriteLine(notes+" has the acceptable character limit");
-            break;
+namespace Notes_Taker_Astonian_Awards
+{
+    public class note{
+        public static string content (string note_){
+            string notes = note_;
+            int char_count = notes.Length;
+            
+            if (char_count>50){
+                string char_ = "The note is beyond the character limits";
+                return char_;
+                note_ = "Nothing";
+                Console.WriteLine(note_);
+            } else
+            {
+                string char_ = notes+" has the acceptable character limit";
+                return char_;
+                note_ = notes;
+                Console.WriteLine(note_);
+            }
         }
     }
 
-    public static check(int check_digit){
+    public static string check(int check_digit, string note_){
         Console.WriteLine("Your check digit is "+check_digit+".");
-
+        
         if (check_digit == 1){
-            Console.WriteLine("You have completed "+note+". It will now delete the note.");
-            note = "None";
-        }elif (check_digit == 2);{
+            Console.WriteLine("You have completed "+note_+". It will now delete the note.");
+            note_ = "None";
+        }else if (check_digit == 2){
             Console.WriteLine("Please finish your activity.");
-            break;
-        }elif(check_digit == 3);{
+        }else if(check_digit == 3){
             Console.WriteLine("Please start your activty. Come back when you`re done.");
-            break;
-        }elif(check_digit > 3 || check_digit < 0);{
+        }else if(check_digit > 3 || check_digit < 0){
             Console.WriteLine("Please insert a correct number.");
-            Restart();
+        }
         }
     }
 }
